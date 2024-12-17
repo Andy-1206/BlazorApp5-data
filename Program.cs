6febe918c680ef0caf813272b626ac56544899a5
Andy-1206 <andy.emsellem@gmail.com>
2024-12-17 16:25:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using BlazorApp5_data.Components;
+
+namespace BlazorApp5_data
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
